577d63c42ea2c644e0ca112b11899bbe1ee903a0
Andrew Paumier <39337835+20apaumier@users.noreply.github.com>
2024-09-29 14:50:01 -0700

    Revert "working on one container!!! :)"

--- frontend/src/components/ChartComponent.tsx
+++ frontend/src/components/ChartComponent.tsx
@@ -57,9 +57,8 @@ const ChartComponent: React.FC<ChartComponentProps> = ({
             if (endDate) params.to = endDate.toISOString().split('T')[0];
 
             // fetch stock data from api
-            // https -> http
             axios
-                .get(`http://localhost:7086/Stock/${stockSymbol}`, { params })
+                .get(`https://localhost:7086/Stock/${stockSymbol}`, { params })
                 .then((response) => {
                     setRawData(response.data); // set raw data from api response
                     setLoading(false); // stop loading
